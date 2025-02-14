b1a8b398d3e7ae2269679b719284bcaac7a6ca2a
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-02-14 16:19:12 -0600

    Update template

--- src/Hooks/BTSCharacterSpawnAnimationController.cpp
+++ src/Hooks/BTSCharacterSpawnAnimationController.cpp
@@ -1,21 +1,32 @@
-#include "Hooking.hpp"
-#include "Logger.hpp"
+#include "autohooks/shared/hooks.hpp"
+#include "logger.hpp"
 
+// GlobalNamespace
 #include "GlobalNamespace/BTSCharacterSpawnAnimationController.hpp"
 #include "GlobalNamespace/BTSCharacterSpawnController.hpp"
 
 namespace Hooks {
     using namespace GlobalNamespace;
 
-    MAKE_LATE_HOOK_MATCH(BTSCharacterSpawnAnimationController_PlayAnimation, &BTSCharacterSpawnAnimationController::PlayAnimation, void, BTSCharacterSpawnAnimationController* self) {
-    BTSCharacterSpawnAnimationController_PlayAnimation(self);
-    self->StopAnimation();
-    Logger.info("BTS character was detected and stopped.");
+    MAKE_LATE_HOOK_MATCH(
+        BTSCharacterSpawnAnimationController_PlayAnimation,
+        &BTSCharacterSpawnAnimationController::PlayAnimation,
+        void,
+        BTSCharacterSpawnAnimationController* self
+    ) {
+        BTSCharacterSpawnAnimationController_PlayAnimation(self);
+        self->StopAnimation();
+        Logger.info("BTS character was detected and stopped.");
     }
 
-    MAKE_LATE_HOOK_MATCH(BTSCharacterSpawnAnimationController_ResumeAnimation, &BTSCharacterSpawnAnimationController::ResumeAnimation, void, BTSCharacterSpawnAnimationController* self) {
-    BTSCharacterSpawnAnimationController_ResumeAnimation(self);
-    self->StopAnimation();
-    Logger.info("BTS character was detected and stopped.");
+    MAKE_LATE_HOOK_MATCH(
+        BTSCharacterSpawnAnimationController_ResumeAnimation,
+        &BTSCharacterSpawnAnimationController::ResumeAnimation,
+        void,
+        BTSCharacterSpawnAnimationController* self
+    ) {
+        BTSCharacterSpawnAnimationController_ResumeAnimation(self);
+        self->StopAnimation();
+        Logger.info("BTS character was detected and stopped.");
     }
-}
+}  // namespace Hooks
